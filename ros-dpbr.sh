3b36e16d6b868bda9374047a29e5c0fc27227426
zhou jielei <zhou-jielei@gmo.jp>
2023-03-29 13:44:49 +0900

    增加3网联合

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -2,15 +2,31 @@
 mkdir -p ./pbr
 cd ./pbr
 
-# AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
+
+wget --no-check-certificate -c -O CN.txt https://gaoyifan.github.io/china-operator-ip/china.txt
+wget --no-check-certificate -c -O chinanet.txt https://gaoyifan.github.io/china-operator-ip/chinanet.txt
+wget --no-check-certificate -c -O cmcc.txt https://gaoyifan.github.io/china-operator-ip/cmcc.txt
+wget --no-check-certificate -c -O unicom.txt https://gaoyifan.github.io/china-operator-ip/unicom.txt
 
 {
 echo "/ip firewall address-list"
 
 for net in $(cat CN.txt) ; do
-  echo "add list=CN address=$net comment=AS4809"
+  echo "add list=CN address=$net comment=china-all"
 done
+
+for net in $(cat chinanet.txt) ; do
+  echo "add list=chinanet address=$net comment=china-net"
+done
+
+for net in $(cat cmcc.txt) ; do
+  echo "add list=cmcc address=$net comment=china-cmcc"
+done
+
+for net in $(cat unicom.txt) ; do
+  echo "add list=unicom address=$net comment=china-unicom"
+done
+
 
 } > ../CN.rsc
 
